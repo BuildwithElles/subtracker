d34fe403a6b82ebc5a600ae7d53cddca68f63950
Buildwithelles <lobeng247@gmail.com>
2025-08-03 16:59:46 +0100

    Add main application structure with routing and Supabase client configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import SignUp from './pages/SignUp'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignUp />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
